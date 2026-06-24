20fb22603d762e026ca100769518756a53d12f35
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2026-06-24 16:20:29 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  let user = null;
+  return (
+    <>
+      <div className="App">
+        <h1 className="font-bold text-3xl">Portfolio Frontend</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
